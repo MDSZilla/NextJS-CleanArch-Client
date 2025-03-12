283253d1b3b03428bfea6982628773d2750b7352
MDSZilla <mdszilla@gmail.com>
2025-03-12 08:47:14 +0500

    Updated to NextJS 15

--- _lib/cookies/getAuthToken.ts
+++ _lib/cookies/getAuthToken.ts
@@ -3,5 +3,5 @@
 import { cookies } from "next/headers";
 
 export default async function getAuthToken() {
-    return cookies().get(process.env.AUTH_COOKIE_NAME!)?.value.toString() || '';
+    return (await cookies()).get(process.env.AUTH_COOKIE_NAME!)?.value.toString() || '';
 };
