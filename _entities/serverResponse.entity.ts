362221ce149dde2acddb2d09d0f93a76fda47539
MDSZilla <mdszilla@gmail.com>
2025-03-12 08:42:42 +0500

    Updated ServerResponseEnttiy to Type and Added Valid Response Statuses

--- _entities/serverResponse.entity.ts
+++ _entities/serverResponse.entity.ts
@@ -1,25 +1,12 @@
-import { ServerResponseStatus } from "@/_enums/serverResponseStatus.enum";
+import { ServerResponseEnum } from "@/_enums/serverResponseStatus.enum";
 
-export class ServerResponseError{
-    repository: string;
-    method: string;
-    error: string;
 
-    constructor(repository: string = "", method: string = "", error: string = ""){
-        this.repository = repository;
-        this.method = method;
-        this.error = error;
-    };
-};
-
-export class ServerResponse<T>{
-    status: ServerResponseStatus;
+export type ServerResponseType<T> = {
     response: T;
-    error: ServerResponseError
-
-    constructor(response: T, status: ServerResponseStatus, error: ServerResponseError = new ServerResponseError()){
-        this.response = response;
-        this.status = status;
-        this.error = error;
-    };
+    error: {
+        message: string;
+        repository: string;
+        method: string;
+    } | null;
+    status: ServerResponseEnum;
 };
--- _enums/serverResponseStatus.enum.ts
+++ _enums/serverResponseStatus.enum.ts
@@ -1,5 +1,15 @@
-export enum ServerResponseStatus{
+export enum ServerResponseEnum{
     SUCCESS = 200,
-    CLIENTERROR = 400,
-    SERVERERROR = 500,
+    CREATED =  201,
+    ACCEPTED = 202,
+    NO_CONTENT = 204,
+
+    BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
+    FORBIDDEN = 403,
+    NOT_FOUND = 404,
+    METHOD_NOT_ALLOWED = 405,
+
+    INTERNAL_SERVER_ERROR =  500,
+    BAD_GATEWAY = 502,
 };
